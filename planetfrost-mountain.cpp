5bf5edaed9e8e26744c462c311b0802ce8f340b9
sudo-d4s3 <honeycutt.chandler@pm.me>
2020-03-06 23:47:50 -0500

    movement
    added character symbol '@' that can move within the 80x24 grid

--- planetfrost-mountain.cpp
+++ planetfrost-mountain.cpp
@@ -16,7 +16,28 @@ int main(int argc, char const *argv[]) {
                 mvhline(i, 0, '.', 80);
         }
         do {
+                mvaddch(y, x, '@');
+                move(y, x);
                 refresh();
+
+
+                int ch = getch();
+                if (ch == 261 and x != 79) { //left arrow
+                        mvaddch(y, x, '.');
+                        x += 1;
+                }
+                if (ch == 260 and x != 0) { //right arrow
+                        mvaddch(y, x, '.');
+                        x -= 1;
+                }
+                if (ch == 259 and y != 0) { //left arrow
+                        mvaddch(y, x, '.');
+                        y -= 1;
+                }
+                if (ch == 258 and y != 23) { //left arrow
+                        mvaddch(y, x, '.');
+                        y += 1;
+                }
         }
         while (1);
 
