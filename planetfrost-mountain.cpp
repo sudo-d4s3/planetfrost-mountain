d0516f7c5a2823abd04b6d4cf0f2910f55da48aa
sudo-d4s3 <honeycutt.chandler@pm.me>
2020-03-06 23:35:19 -0500

    Background
    background for a 80x24 character terminal

--- planetfrost-mountain.cpp
+++ planetfrost-mountain.cpp
@@ -2,6 +2,23 @@
 
 
 int main(int argc, char const *argv[]) {
-        
+        int x = 0;
+        int y = 0;
+
+
+        initscr();
+        noecho();
+        cbreak();
+        curs_set(FALSE);
+        keypad(stdscr, TRUE);
+
+        for (size_t i = 0; i < 24; i++) {
+                mvhline(i, 0, '.', 80);
+        }
+        do {
+                refresh();
+        }
+        while (1);
+
         return 0;
 }
