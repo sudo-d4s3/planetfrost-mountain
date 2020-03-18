fe0511d85712155819c50845c312925c70688fdc
sudo-d4s3 <honeycutt.chandler@pm.me>
2020-03-18 01:13:43 -0400

    minor fixes and delete crops
    changed headers to make it compatible with windows, made new array for 
    crop manipulation, removed residual code from the first map gen

--- draw_map.cpp
+++ draw_map.cpp
@@ -1,8 +1,7 @@
-#include <ncurses.h>
+#include <ncursesw/curses.h>
 
 char draw_map(char map[24][80]) {
         for (size_t i = 0; i < 24; i++) {
-                //mvhline(i, 0, '.', 80)
                 for (size_t i2 = 0; i2 < 80; i2++) {
                         map[i][i2] = '.';
                         mvhline(i, i2, map[i][i2], 1);
@@ -13,7 +12,6 @@ char draw_map(char map[24][80]) {
 
 char redraw_map(char map[24][80]) {
         for (size_t i = 0; i < 24; i++) {
-                //mvhline(i, 0, '.', 80)
                 for (size_t i2 = 0; i2 < 80; i2++) {
                         mvhline(i, i2, map[i][i2], 1);
                 }
--- planetfrost-mountain.cpp
+++ planetfrost-mountain.cpp
@@ -1,10 +1,11 @@
-#include <ncurses.h>
+#include <ncursesw/curses.h>
 #include "draw_map.cpp"
 
 int main(int argc, char const *argv[]) {
-        int x = 0;
-        int y = 0;
+        int x = 40;
+        int y = 12;
         char map[24][80];
+        char crops[24][80];
 
         initscr();
         noecho();
@@ -24,28 +25,30 @@ int main(int argc, char const *argv[]) {
                         break;
                 }
                 if (ch == 261 and x != 79) { //left arrow
-                        mvaddch(y, x, '.');
                         x += 1;
                         redraw_map(map);
                 }
                 if (ch == 260 and x != 0) { //right arrow
-                        mvaddch(y, x, '.');
                         x -= 1;
                         redraw_map(map);
                 }
                 if (ch == 259 and y != 0) { //left arrow
-                        mvaddch(y, x, '.');
                         y -= 1;
                         redraw_map(map);
                 }
                 if (ch == 258 and y != 23) { //left arrow
-                        mvaddch(y, x, '.');
                         y += 1;
                         redraw_map(map);
                 }
-                if (ch == 32){
-                        //mvaddch(y + 1, x, '^');
-                        map[y][x] = '^';
+                if (ch == 32){ //plant crop
+                        crops[y][x] = '*';
+                        map[y][x] = crops[y][x];
+                }
+                if (ch == 104){ //harvest crop
+                        if (crops[y][x] == '*'){
+                                crops[y][x] = ' ';
+                                map[y][x] = '.';
+                        }
                 }
         }
         while (1);
